82c1757a2e72721a4b2f1adcaa1c971e0dbf926d
habib khan <habib134khan@gmail.com>
2024-09-04 04:46:19 +0600

    Update Angular project files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component'; 
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component'; 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: EmployeesComponent },
+  { path: 'add-employee', component: AddEmployeeComponent },
+  { path: 'edit-employee/:id', component: EditEmployeeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeesComponent } from './components/employees/employees.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddEmployeeComponent } from './components/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+
+const appRoutes: Routes = [
+  { path: '', component: EmployeesComponent },
+  { path: 'add-employee', component: AddEmployeeComponent },
+  { path: 'edit-employee/:id', component: EditEmployeeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeesComponent,
+    NavbarComponent,
+    AddEmployeeComponent,
+    EditEmployeeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
